8e2e0221e06f7b5eda1247e6c5737cc1f4d1e79a
Student <camyngo@byu.edu>
2021-01-26 17:00:18 -0700

    update test

--- src/test/java/com/makotojava/learn/hellojunit5/solution/DijkstraTest.java
+++ src/test/java/com/makotojava/learn/hellojunit5/solution/DijkstraTest.java
@@ -55,7 +55,7 @@ public class DijkstraTest {
     }
 
     @Test
-    @DisplayName("Testing Dijkstra Graph")
+    @DisplayName("Testing Positive Number Dijkstra Graph")
     public void testShortestPath() {
         assertNotNull(classUnderTest);
         int [][] graph = { { 0, 2, 4, Dijkstra.M, Dijkstra.M, Dijkstra.M }, { 2, 0, 1, 4, Dijkstra.M, Dijkstra.M },
@@ -63,7 +63,41 @@ public class DijkstraTest {
                 { Dijkstra.M, Dijkstra.M, Dijkstra.M, 3, 5, 0 } };
         Dijkstra d = new Dijkstra(graph);
         int sp = d.shortestPath(0, 4);
-        assertEquals(sp, 6);
+        assertAll(
+            () -> {assumeTrue(sp > 0);},
+            () -> {assertEquals(sp, 6);});
+    }
+
+    @Nested
+    @DisplayName("When graph has number < 0")
+    class MixedNumbersTest {
+
+
+        @BeforeEach
+        public void setUp() throws Exception {
+          classUnderTest = new Dijkstra(g);
+        }
+    
+        @AfterEach
+        public void tearDown() throws Exception {
+          classUnderTest = null;
+        }
+
+        @Test
+        @DisplayName("tests with positive and negative numbers")
+        public void testShortestPath(){
+            assertNotNull(classUnderTest);
+            final int[][] graph = { { 0, -2, -4, Dijkstra.M, Dijkstra.M, Dijkstra.M }, { 2, 0, 1, 4, Dijkstra.M, Dijkstra.M },
+            { 4, -1, 0, 2, 6, Dijkstra.M }, { Dijkstra.M, -4, 2, 0, -1, -3 }, { Dijkstra.M, Dijkstra.M, 6, -1, 0, -5 },
+            { Dijkstra.M, Dijkstra.M, Dijkstra.M, 3, -5, 0 } };
+        Dijkstra d = new Dijkstra(graph);
+        int sp = d.shortestPath(0, 5);
+        assumeTrue(sp > 0);
+
+        }
+
+
+
     }
 
 
